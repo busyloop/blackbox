d1de5a9333553e1579ea3468fe352aa2d5731c4e
moe <moe@busyloop.net>
2015-11-22 18:30:28 +0100

    Pass through input in case of exception

--- lib/blackbox/number.rb
+++ lib/blackbox/number.rb
@@ -74,8 +74,7 @@ module BB
              ).sub(/(#{escaped_separator})(\d*[1-9])?0+\z/, '\1\2').sub(/#{escaped_separator}\z/, '')
              storage_units_format.gsub(/%n/, formatted_number).gsub(/%u/, unit)
            rescue => e
-             e
-             #number
+             number
            end
          end
        end
