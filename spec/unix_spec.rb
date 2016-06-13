09f2820039ebaca9a0c9e3b6b4a4a150c78b6adc
moe <moe@busyloop.net>
2016-06-13 02:13:10 +0200

    Rubocop cleanup

--- spec/unix_spec.rb
+++ spec/unix_spec.rb
@@ -5,29 +5,29 @@ require 'blackbox/unix'
 describe BB::Unix do
   describe '.run_each' do
     it 'exits on failure by default' do
-      expect {
-        expect {
+      expect do
+        expect do
           BB::Unix.run_each('false')
-        }.to output().to_stdout
-      }.to raise_error(SystemExit)
+        end.to output.to_stdout
+      end.to raise_error(SystemExit)
     end
 
     it 'echos to stdout by default' do
-      expect {
+      expect do
         BB::Unix.run_each('true')
-      }.to output(/true/).to_stdout
+      end.to output(/true/).to_stdout
     end
 
     it 'quiet success in quiet mode' do
-      expect {
+      expect do
         BB::Unix.run_each(":return\n:quiet\ntrue")
-      }.to output('').to_stdout
+      end.to output('').to_stdout
     end
 
     it 'loud failure in quiet mode' do
-      expect {
+      expect do
         BB::Unix.run_each(":return\n:quiet\nfalse")
-      }.to output(/exit 1: false/).to_stdout
+      end.to output(/exit 1: false/).to_stdout
     end
   end
 end
