17cda77f68273f8f48180c7ec14add26295d6eb2
moe <moe@busyloop.net>
2016-12-25 17:06:42 +0100

    Make rubocop happy

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -14,48 +14,48 @@ OPENSSL_CIPHERS = %w(aes-128-cbc aes-128-ecb aes-192-cbc aes-192-ecb aes-256-cbc
                   %w(rc4 rc4-40)
 
 CIPHER_KEY_IV_SIZE = {
-  'aes-128-cbc': [16,16],
-  'aes-128-ecb': [16,1],
-  'aes-192-cbc': [24,16],
-  'aes-192-ecb': [24,1],
-  'aes-256-cbc': [32,16],
-  'aes-256-ecb': [32,1],
-  'bf': [16,8],
-  'bf-cbc': [16,8],
-  'bf-cfb': [16,8],
-  'bf-ecb': [16,1],
-  'bf-ofb': [16,8],
-  'cast': [16,8],
-  'cast-cbc': [16,8],
-  'cast5-cbc': [16,8],
-  'cast5-cfb': [16,8],
-  'cast5-ecb': [16,1],
-  'cast5-ofb': [16,8],
-  'des': [8,8],
-  'des-cbc': [8,8],
-  'des-cfb': [8,8],
-  'des-ecb': [8,1],
-  'des-ede': [16,1],
-  'des-ede-cbc': [16,8],
-  'des-ede-cfb': [16,8],
-  'des-ede-ofb': [16,8],
-  'des-ede3': [24,1],
-  'des-ede3-cbc': [24,8],
-  'des-ede3-cfb': [24,8],
-  'des-ede3-ofb': [24,8],
-  'des-ofb': [8,8],
-  'des3': [24,8],
-  'desx': [24,8],
-  'rc2': [16,8],
-  'rc2-40-cbc': [5,8],
-  'rc2-64-cbc': [8,8],
-  'rc2-cbc': [16,8],
-  'rc2-cfb': [16,8],
-  'rc2-ecb': [16,1],
-  'rc2-ofb': [16,8],
-  'rc4': [16,1],
-  'rc4-40': [5,1]
-}
+  'aes-128-cbc': [16, 16],
+  'aes-128-ecb': [16, 1],
+  'aes-192-cbc': [24, 16],
+  'aes-192-ecb': [24, 1],
+  'aes-256-cbc': [32, 16],
+  'aes-256-ecb': [32, 1],
+  'bf': [16, 8],
+  'bf-cbc': [16, 8],
+  'bf-cfb': [16, 8],
+  'bf-ecb': [16, 1],
+  'bf-ofb': [16, 8],
+  'cast': [16, 8],
+  'cast-cbc': [16, 8],
+  'cast5-cbc': [16, 8],
+  'cast5-cfb': [16, 8],
+  'cast5-ecb': [16, 1],
+  'cast5-ofb': [16, 8],
+  'des': [8, 8],
+  'des-cbc': [8, 8],
+  'des-cfb': [8, 8],
+  'des-ecb': [8, 1],
+  'des-ede': [16, 1],
+  'des-ede-cbc': [16, 8],
+  'des-ede-cfb': [16, 8],
+  'des-ede-ofb': [16, 8],
+  'des-ede3': [24, 1],
+  'des-ede3-cbc': [24, 8],
+  'des-ede3-cfb': [24, 8],
+  'des-ede3-ofb': [24, 8],
+  'des-ofb': [8, 8],
+  'des3': [24, 8],
+  'desx': [24, 8],
+  'rc2': [16, 8],
+  'rc2-40-cbc': [5, 8],
+  'rc2-64-cbc': [8, 8],
+  'rc2-cbc': [16, 8],
+  'rc2-cfb': [16, 8],
+  'rc2-ecb': [16, 1],
+  'rc2-ofb': [16, 8],
+  'rc4': [16, 1],
+  'rc4-40': [5, 1]
+}.freeze
 
 TEST_KEY = '12345678901234567890123456789012'
 TEST_IV = 'x234567890123456789012345678901x'
@@ -71,26 +71,22 @@ describe BB::Crypto do
         m_enc, m_dec = e
         describe "#{m_enc}, #{m_dec}" do
           it 'can decrypt what it encrypted (short string, random iv)' do
-            test_key = TEST_KEY[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[0]-1]
-            test_iv = TEST_IV[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[1]-1]
-            test_iv = nil if test_iv.length == 1
+            test_key = TEST_KEY[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[0] - 1]
             ct = BB::Crypto.send(m_enc, TEST_TEXT_SHORT, test_key, cipher)
             pt = BB::Crypto.send(m_dec, ct, test_key, cipher)
             expect(pt).to eq(TEST_TEXT_SHORT)
           end
 
           it 'can decrypt what it encrypted (long string, random iv)' do
-            test_key = TEST_KEY[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[0]-1]
-            test_iv = TEST_IV[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[1]-1]
-            test_iv = nil if test_iv.length == 1
+            test_key = TEST_KEY[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[0] - 1]
             ct = BB::Crypto.send(m_enc, TEST_TEXT_LONG, test_key, cipher)
             pt = BB::Crypto.send(m_dec, ct, test_key, cipher)
             expect(pt).to eq(TEST_TEXT_LONG)
           end
 
           it 'can decrypt what it encrypted (long string, static iv)' do
-            test_key = TEST_KEY[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[0]-1]
-            test_iv = TEST_IV[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[1]-1]
+            test_key = TEST_KEY[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[0] - 1]
+            test_iv = TEST_IV[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[1] - 1]
             test_iv = nil if test_iv.length == 1
             ct = BB::Crypto.send(m_enc, TEST_TEXT_LONG, test_key, cipher, test_iv)
             pt = BB::Crypto.send(m_dec, ct, test_key, cipher, test_iv)
@@ -98,8 +94,8 @@ describe BB::Crypto do
           end
 
           it 'returns consistent output with static iv' do
-            test_key = TEST_KEY[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[0]-1]
-            test_iv = TEST_IV[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[1]-1]
+            test_key = TEST_KEY[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[0] - 1]
+            test_iv = TEST_IV[0..CIPHER_KEY_IV_SIZE.fetch(cipher.to_sym)[1] - 1]
             test_iv = nil if test_iv.length == 1
             a = BB::Crypto.send(m_enc, TEST_TEXT_SHORT, test_key, cipher, test_iv)
             b = BB::Crypto.send(m_enc, TEST_TEXT_SHORT, test_key, cipher, test_iv)
